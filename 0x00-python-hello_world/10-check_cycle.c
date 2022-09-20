13db83bf5515b12ce0f7caefaeaf860f8c20395b
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-09-20 02:59:56 -0700

    fixed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,64 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
+#include <stdlib.h>                                                                                                                              
 
-/**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
+#include "lists.h"                                                                                                                               
 
-{
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+                                                                                                                                                 
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
+/**                                                                                                                                              
 
-    return (n);
-}
+ * check_cycle - it checked if a singly linked list contain a cycle                                                                              
 
-/**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to a pointer of the start of the list
- * @n: integer to be included in node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-    listint_t *new;
+ * @list: a singly linked list                                                                                                                   
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+ * return: if their is not cycle -0                                                                                                              
 
-    new->n = n;
-    new->next = *head;
-    *head = new;
+ *      if their is a cycle -1                                                                                                                   
 
-    return (new);
-}
+ */                                                                                                                                              
 
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
+                                                                                                                                                 
 
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
-}
+int check_cycle(listint_t *list)                                                                                                                 
+
+{                                                                                                                                                
+
+        listint_t *slow, *fast;                                                                                                                  
+
+                                                                                                                                                 
+
+        if (list == NULL || list->next == NULL)                                                                                                  
+
+                return (0);                                                                                                                      
+
+                                                                                                                                                 
+
+        slow = list->next;                                                                                                                       
+
+        fast = list->next->next;                                                                                                                 
+
+                                                                                                                                                 
+
+        while (slow && fast && fast->next)                                                                                                       
+
+        {                                                                                                                                        
+
+                if (slow == fast)                    
+                        return (1);
+                slow = slow->next;
+                fast = fast->next>next;                                                   }
+        return (0);
+}                   
