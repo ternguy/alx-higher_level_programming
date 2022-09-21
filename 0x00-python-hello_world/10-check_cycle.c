1ea0c6aa5c146f1a734f756b577d021befaf7784
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-09-21 06:09:16 -0700

    done

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,50 +1,26 @@
-#include <stdlib.h>                                                                                                                              
+#include "lists.h"
+#include <stdlib.h>
+/** 
+ * check_cycle - it checks if a singly linked list contain a cycle
+ * @list: a singly linked list
+ * return: o if their is no cycle and 1 if their is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *three, *four;
 
-#include "lists.h"                                                                                                                               
+	if(list == NULL|| list->next == NULL)
+		return (0);
+	three = list->next;
+	four = list->next->next;
 
-                                                                                                                                                 
+	while (three && four && four->next)
+	{
+		if (three == four)
+			return(1);
+		three = three->next;
+		four = four->next->next;
+	}
+	return (0);
 
-/**                                                                                                                                              
-
- * check_cycle - it checked if a singly linked list contain a cycle                                                                              
-
- * @list: a singly linked list                                                                                                                   
-
- * return: if their is not cycle -0                                                                                                              
-
- *      if their is a cycle -1                                                                                                                   
-
- */                                                                                                                                              
-
-                                                                                                                                                 
-
-int check_cycle(listint_t *list)                                                                                                                 
-
-{                                                                                                                                                
-
-        listint_t *slow, *fast;                                                                                                                  
-
-                                                                                                                                                 
-
-        if (list == NULL || list->next == NULL)                                                                                                  
-
-                return (0);                                                                                                                      
-
-                                                                                                                                                 
-
-        slow = list->next;                                                                                                                       
-
-        fast = list->next->next;                                                                                                                 
-
-                                                                                                                                                 
-
-        while (slow && fast && fast->next)                                                                                                       
-
-        {                                                                                                                                        
-
-                if (slow == fast)                    
-                        return (1);
-                slow = slow->next;
-                fast = fast->next>next;                                                   }
-        return (0);
-}                   
+}
