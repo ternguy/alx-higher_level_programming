056a40f287c60a22d75aaf8243f46f4a6ec556d6
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-09-20 23:43:37 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 p = 0
-for g in range(ord('z'), ord('a') -1, -1):
+for g in range(ord('z'), ord('a') - 1, -1):
     print('{}'.format(chr(g - p)), end='')
     p = 32 if p == 0 else 0
