abacd05ceb43d7d88dc1cc6e878a09567de407d5
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-09-22 07:27:49 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """print the sum, product, difference, quotion of 10 and 5. """
+    from calculator_1 import sub, mul, div, add
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
