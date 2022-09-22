ebb225a351753bb74b51ab8e75eb49efdaf9e2cf
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-09-22 07:30:18 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """print the sum, product, difference, quotion of 10 and 5. """
-    from calculator_1 import sub, mul, div, add
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
 
