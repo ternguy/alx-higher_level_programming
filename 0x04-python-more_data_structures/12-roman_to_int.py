362453928ae296765b46a4c565648a92107e011a
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-09-28 08:55:59 -0700

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    total_of_all = 0
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = digits[roman]
+        total_of_all += arabic if total_of_all < arabic * 5 else -arabic 
+    return total_of_all
