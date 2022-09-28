3f79227f0ae0aec0a8c99ea2af900fde6b430cbc
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-09-28 09:06:24 -0700

    11-multiply_list_map.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,5 +7,5 @@ def roman_to_int(roman_string):
     digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for roman in reversed(roman_string):
         arabic = digits[roman]
-        total_of_all += arabic if total_of_all < arabic * 5 else -arabic 
+        total_of_all += arabic if total_of_all < arabic * 5 else -arabic
     return total_of_all
