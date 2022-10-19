abf2375b6c152c524fac68faca0460442933604b
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-10-19 04:00:14 -0700

    done

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 """"
-
 This module contains a function that prints a name
-
-
 """
-
 
 def say_my_name(first_name, last_name=""):
     '''This function prints name (<first name> <last name>)
