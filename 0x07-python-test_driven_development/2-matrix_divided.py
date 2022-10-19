05722fedc20ab32162922f49332b27054f5ef8fe
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-10-19 07:15:03 -0700

    done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -32,6 +32,7 @@ def matrix_divided(matrix, div):
 
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
