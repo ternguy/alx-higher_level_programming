e274a85c5a1cdcf9026938f68d4959cac60f7334
AGBAGHER JAPHET <ternguy1995@gmail.com>
2023-02-09 03:44:32 -0800

    done

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 
-
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const fileDir = process.argv[3];
+const filePath = process.argv[3];
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else {
-    fs.writeFile(fileDir, body, 'utf-8');
+    fs.writeFile(filePath, body, 'utf-8');
   }
 });
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-
 const request = require('request');
 const url = process.argv[2];
 
